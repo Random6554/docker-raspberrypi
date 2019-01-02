ce0f07d9160cd9245bcbb86f120c48d0fe3887d9
Random6554 <39579011+Random6554@users.noreply.github.com>
2019-01-02 13:47:35 +0000

    Update docker-kill-all.sh

--- docker-kill-all.sh
+++ docker-kill-all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-stop all containers:
+#stop all containers:
 docker kill $(docker ps -q)
 
-remove all containers
+#remove all containers
 docker rm $(docker ps -a -q)
 
-remove all docker images
+#remove all docker images
 docker rmi $(docker images -q)
